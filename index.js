fd30cd77c15912c60085480144e3772e55893318
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-22 15:55:15 -0600

    Implementado controller de anime para reduccion de logica en el index.

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ const app = express();
 const Home = require('./Rutas.json');
 
 const newsRouter = require('./controllers/news');
-
-
-/*/  Animes  */
-const LastEpisodes = require('./Script/Animes/LastEpisodes.js');
-const  MoreInfo  = require('./Script/Animes/MoreInfo.js');
-const  VerEpisodio = require('./Script/Animes/VerEpisodio.js');
-const Search  = require('./Script/Animes/Search.js');
-const Directorio  = require('./Script/Animes/Directorio.js');
-const ActualizarDirectorio  = require('./Script/Animes/ActualizarDirectorio.js');
-const DirecQuery  = require('./Script/Animes/DirecQuery.js');
-
+const animesRouter = require('./controllers/animes');
 
 
 app.get('/', (req, res) => {
@@ -22,68 +12,10 @@ app.get('/', (req, res) => {
 
 app.use('/api/news', newsRouter);
 
-
-app.get('/api/anime/lastepisodes', (req, res) => {
-	LastEpisodes().then(data => res.json(data));
-});
-
-
-app.get('/api/anime/directorio', (req, res) => {
-	Directorio().then(data => res.json(data));
-});
-
-
-app.get('/api/anime/directorio/actualizar', (req, res) => {
-	ActualizarDirectorio().then(data => res.json(data));
-});
-
-app.get('/api/anime/directorio/:query', (req, res) => {
-	DirecQuery(req.params.query).then(data => res.json(data));
-});
-
-app.get('/api/anime/:opcion/:id', (req, res) => {
-	switch (req.params.opcion) {
-	
-	case 'moreInfo':
-		MoreInfo(req.params.id).then(animes => {
-			res.json(animes);
-		});
-		break;
-
-	case 'verEpisodio':
-            
-		VerEpisodio(req.params.id).then(animes => {   
-			res.json(animes);
-		});
-		break;
-
-	case 'search':
-		Search(req.params.id).then(animes => {                      
-			res.json(animes);
-		});
-		break;
-
-	default:
-		res.send('No se encontro la opcion').end();
-		break;
-    
-	}      
-
-
-
-
-
-
-
-
-
-});
-
-
+app.use('/api/anime', animesRouter);
 
 
 const port = 3001;
-
 app.listen(port, () => {
 	console.log(`Ya se levanto (el servidor)🥵 http://localhost:${port}`);
 });
