ed90a5f465e527dddc5b09f144b29e1325d54182
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-22 15:53:52 -0600

    Facilita el uso de los scrips de animes

---
+++ Script/Animes/index.js
@@ -0,0 +1,17 @@
+const ActualizarDir = require('./ActualizarDirectorio');
+const DirQuery = require('./DirecQuery');
+const Directorio = require('./Directorio');
+const LastEpisodes = require('./lastEpisodes');
+const MoreInfo = require('./MoreInfo');
+const Search = require('./Search');
+const VerEpisodio = require('./VerEpisodio');
+
+module.exports = {
+	ActualizarDir,
+	DirQuery,
+	Directorio,
+	LastEpisodes,
+	MoreInfo,
+	Search,
+	VerEpisodio
+};
