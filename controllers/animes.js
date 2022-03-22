f86a49b673fc5eaf78304c706f17f8c55315b4a5
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-22 15:52:59 -0600

    Se agrego controlador para rutas de anime

---
+++ controllers/animes.js
@@ -0,0 +1,56 @@
+const animesRouter = require('express').Router();
+
+const { ActualizarDir, 
+	DirQuery, 
+	Directorio, 
+	LastEpisodes, 
+	MoreInfo, 
+	Search, 
+	VerEpisodio 
+} = require('../Script/Animes');
+
+
+animesRouter.get('/lastepisodes', async (req, res) => {
+	await LastEpisodes().then(data => res.json(data));
+});
+
+animesRouter.get('/directorio', async (req, res) => {
+	await Directorio().then(data => res.json(data));
+});
+
+animesRouter.get('/directorio/actualizar', async (req, res) => {
+	await ActualizarDir().then(data => res.json(data));
+});
+
+animesRouter.get('/directorio/:query', async (req, res) => {
+	await DirQuery(req.params.query).then(data => res.json(data));
+});
+
+animesRouter.get('/:opcion/:id', async (req, res) => {
+	switch (req.params.opcion) {
+	
+	case 'moreInfo':
+		await MoreInfo(req.params.id).then(animes => {
+			res.json(animes);
+		});
+		break;
+
+	case 'verEpisodio':
+		await VerEpisodio(req.params.id).then(animes => {   
+			res.json(animes);
+		});
+		break;
+
+	case 'search':
+		await Search(req.params.id).then(animes => {                      
+			res.json(animes);
+		});
+		break;
+
+	default:
+		res.send('No se encontro la opcion').end();
+		break;
+	}      
+});
+
+module.exports = animesRouter;
