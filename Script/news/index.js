c2c845205419ed33adeb9d385f604eeef058df37
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-21 20:38:23 -0600

    Este archivo exporta los scrips para llamarlos mas facilmente

---
+++ Script/news/index.js
@@ -0,0 +1,9 @@
+const reviews = require('./Reviews');
+const masNoticias = require('./MasNoticias');
+const populares = require('./Populares');
+const recientes = require('./Recientes');
+
+exports.Reviews = reviews;
+exports.MasNoticias = masNoticias;
+exports.Populares = populares;
+exports.Recientes = recientes;
