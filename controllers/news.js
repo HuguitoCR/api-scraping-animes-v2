d34944e8461e2d461b01ee057da3b7000b809de6
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-22 11:35:11 -0600

    Controllers agregado. Ayuda a disminuir la complejidad de codigo en el index

---
+++ controllers/news.js
@@ -0,0 +1,38 @@
+const newsRouter = require('express').Router();
+
+/*/ Noticias */
+const { Reviews, Populares, MasNoticias, Recientes } = require('../Script/News')
+
+newsRouter.get('/:categoria', async (req , res) => {
+  switch (req.params.categoria) {
+    case 'masNoticias':
+      await MasNoticias().then(noticias => {
+        res.json(noticias);
+      });
+      break;
+
+    case 'populares':
+      await Populares().then(noticias => {
+        res.json(noticias);
+      });
+      break;  
+
+    case 'recientes':
+      await Recientes().then(noticias => {
+        res.json(noticias);
+      });
+      break;
+    
+    case 'reviews':
+      await Reviews().then(noticias => {
+        res.json(noticias);
+      });
+      break;
+
+    default:
+        res.send('No se encontro la categoria').end();
+     break;   
+  }
+});
+
+module.exports = newsRouter;
