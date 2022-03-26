5e7e7c8c3890bf135eb63b172ad748b45963eb83
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-25 23:09:10 -0600

    se cambiaron las constantes individuales por objetos

--- app/Script/Animes/MoreInfo.js
+++ app/Script/Animes/MoreInfo.js
@@ -1,36 +1,31 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const { getHtml } = require('../../helpers');
 
 const MoreInfo = async(Anime, res) => {
-	const url = `https://www.animefenix.com/${Anime}`;
-	const repuesta = await axios.get(url);
-	const datos = cheerio.load(repuesta.data);
+	const $ = await getHtml(`https://www.animefenix.com/${Anime}`);
 	const info = [];
 	const episodes = [];
 
-
-	datos('div.columns.is-mobile.is-multiline').is(function() {
-		const img = datos(this).find('img').attr('src');
-		const estado = datos(this).find('ul.has-text-light').text().split('\n')[2].split(':')[1].replace(' ', '');
-		let siguienteEpisodio;
-		try {
-			siguienteEpisodio = datos(this).find('ul.has-text-light').children('li').children('span.has-text-orange').parent().text().split(':')[1].trim();
-		}
-		catch {
-			siguienteEpisodio = null;
-		}
-		const titulo = datos(this).find('h1.title').text();
-		const sinopsis = datos(this).find('p.sinopsis').text().replace(/\n/g, '');
-		const generos = datos(this).find('a.button.is-small').text().replace('Á', 'A').replace('Fic', 'fic').split(/(?=[A-Z])/).toString().replace('Ang', 'Áng').replace('fic', 'Fic').split(',');
-		const tipo = datos(this).find('ul.has-text-light').text().split('\n')[1].split(':')[1].trimStart();
-		const totalEpisodios = datos(this).find('ul.has-text-light').text().split('\n')[3].split(':')[1].trimStart();
-		info.push({ img, tipo, estado, totalEpisodios, siguienteEpisodio, titulo, sinopsis, generos });
+	$('div.columns.is-mobile.is-multiline').is(function() {
+		const verifyNext = $(this).find('ul.has-text-light').children('li').children('span.has-text-orange').parent().text().split(':')[1];
+		const anime = {
+			img: $(this).find('img').attr('src'),
+			type: $(this).find('ul.has-text-light').text().split('\n')[1].split(':')[1].trimStart(),
+			state: $(this).find('ul.has-text-light').text().split('\n')[2].split(':')[1].replace(' ', ''),
+			total_episodes: $(this).find('ul.has-text-light').text().split('\n')[3].split(':')[1].trimStart(),
+			next_episode:	verifyNext ? verifyNext.trim() : null,
+			title: $(this).find('h1.title').text(),
+			description: $(this).find('p.sinopsis').text().replace(/\n/g, ''),
+			genres: $(this).find('a.button.is-small').text().replace('Á', 'A').replace('Fic', 'fic').split(/(?=[A-Z])/).toString().replace('Ang', 'Áng').replace('fic', 'Fic').split(','),
+		};
+		info.push(anime);
 	});
 
-	datos('ul.anime-page__episode-list li').each(function() {
-		const episodio = parseInt(datos(this).find('a').children('span').text().split(' ')[1]);
-		const link = datos(this).find('a').attr('href').split('https://www.animefenix.com/ver/')[1];
-		episodes.push({ episodio, link });
+	$('ul.anime-page__episode-list li').each(function() {
+		const episode = {
+			number: parseInt($(this).find('a').children('span').text().split(' ')[1]),
+			episode_id: $(this).find('a').attr('href').split('https://www.animefenix.com/ver/')[1],
+		};
+		episodes.push(episode);
 	});
 
 	res.json({ info, episodes });
