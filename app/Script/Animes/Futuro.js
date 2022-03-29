6a5337c6dfdc668dcd4ea769c98908895d00b9d3
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-28 19:21:20 -0600

    Se cambio el uso de multiples const a un objeto

--- app/Script/Animes/Futuro.js
+++ app/Script/Animes/Futuro.js
@@ -1,23 +1,18 @@
 const { getHtml } = require('../../helpers');
 
-
-
 const DirectorioFuturo = async(res) => {
 	const $ = await getHtml('https://myanimelist.net/anime/season/later');
 	const Futuros = [];
-	$('div.seasonal-anime').is(function() {
-		const title = $(this).find('a.link-title').text().trim();
-		let img = '';
-		if ($(this).find('img').attr('src')) {
-			img = $(this).find('img').attr('src');
-		}
-		else {
-			img = $(this).find('img').attr('data-src');
-		}
 
-		const link = $(this).find('a').attr('href');
+	$('div.seasonal-anime').each(function() {
+		const verifyImg = $(this).find('img').attr('src');
+		const anime = {
+			title: $(this).find('a.link-title').text().trim(),
+			img: verifyImg ? verifyImg : $(this).find('img').attr('data-src'),
+			link: $(this).find('a').attr('href'),
+		};
 		
-		Futuros.push({ title, img, link });
+		Futuros.push(anime);
 	});
 
 	res.json({ Futuros });
