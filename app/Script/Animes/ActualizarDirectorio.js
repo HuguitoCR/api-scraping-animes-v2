cb82a05394db0e860768f64d9470298737c872cc
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-29 20:00:18 -0600

    Ya no se utiliza una funcion recursiva y se ha eliminado un llamado de funcion no necesario

--- app/Script/Animes/ActualizarDirectorio.js
+++ app/Script/Animes/ActualizarDirectorio.js
@@ -1,47 +1,32 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const { getHtml } = require('../../helpers');
 const Redis = require('ioredis');
 
 const ActualizarDirectorio = async(res) => {
 	const client = new Redis(process.env.REDIS_URL);
-	const url = 'https://www.animefenix.com/animes?page=';
 	const Directorio = [];
-	const inicio = 1;
 
-	const getData = async () => {
-		const response = await axios(url + inicio);
-		const $ = cheerio.load(response.data);
-		const $paginacion = $('li').last().prev().text();
+	const element = await getHtml('https://www.animefenix.com/animes?page=1');
+	const paginacion = element('li').last().prev().text();
 
-		const getAnimes = async (page) => {
-			const respu = await axios(url + page);
-			const HTML = cheerio.load(respu.data);
-
-			HTML('.list-series .serie-card', respu.data).each(function() {
-				const id = HTML(this).find('a').attr('href').split('https://www.animefenix.com/')[1];
-				const title = HTML(this).find('a.has-text-orange').text().split('\n').join('');
-				const img = HTML(this).find('img').attr('src');
-				const año = HTML(this).find('span.year').text();
-				const estado = HTML(this).find('span.is-orange').text();
-				const tipo = HTML(this).find('span.type').text();
-				const descripcion = HTML(this).find('p').text().split('\n').join('').split('"').join('');
-				Directorio.push({ id, title, img, año, estado, tipo, descripcion });
-			});
-
-
-			if (page < $paginacion) {
-				getAnimes(page + 1);
-			}
-			else {
-				client.set('directorio', JSON.stringify(Directorio));
-				client.quit();
-				res.json({ status: 'ok' });
-			}
-		};
-		getAnimes(inicio);
-	};
-	getData();
-
+	for (let i = 0 ; i<=paginacion; i++){
+		const $ = await getHtml(`https://www.animefenix.com/animes?page=${i}`);
+		$('.list-series .serie-card').each(function() {
+			const anime = {
+				id: $(this).find('a').attr('href').split('https://www.animefenix.com/')[1],
+				title: $(this).find('a.has-text-orange').text().split('\n').join(''),
+				img: $(this).find('img').attr('src'),
+				year: $(this).find('span.year').text(),
+				state: $(this).find('span.is-orange').text(),
+				type: $(this).find('span.type').text(),
+				description: $(this).find('p').text().split('\n').join('').split('"').join(''),
+			};
+			Directorio.push(anime);
+		});
+	}
+		
+	client.set('directorio', JSON.stringify(Directorio));
+	client.quit();
+	res.json({ status: 'ok' });
 };
 
 module.exports = ActualizarDirectorio;
