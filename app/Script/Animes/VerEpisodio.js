625a0c3096fbd5650941200430a6aef2621da4f4
Kevin Arias <kevinjosuearias@hotmail.com>
2022-04-02 21:39:23 -0600

    ahora se implementa getHtml

--- app/Script/Animes/VerEpisodio.js
+++ app/Script/Animes/VerEpisodio.js
@@ -1,36 +1,27 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-
+const { getHtml } = require('../../helpers');
 
 const VerEpisodio = async (id, res) => {
+	const $ = await getHtml(`https://www.animefenix.com/ver/${id}`);
 	const lista =[];
-	
-	const u = `https://www.animefenix.com/ver/${id}`;
-	const response = await axios(u);
-	const $ = cheerio.load(response.data);
 
-	const Id = $('.column .is-6-desktop').find('a').attr('href').split('https://www.animefenix.com/')[1];
+	const anime_id = $('.column .is-6-desktop').find('a').attr('href').split('https://www.animefenix.com/')[1];
+	const servers = $('ul.episode-page__servers-list > li').toArray().map((element) => $(element).text().trim());
+	const serverURL = $('.player-container').find('script').html().match(/(?<=src=["'])([^"'])*/gm);
 
-	const Nombre = $('ul.episode-page__servers-list > li').toArray().map((element) => $(element).text().trim());
-	const URL = $('.player-container').find('script').html().match(/(?<=src=["'])([^"'])*/gm);
-
-	
-	Nombre.forEach(async(element, index) => {
-		const data = await axios(URL[index].replace('amp;', ''));
-		const $$ = cheerio.load(data.data);
+	servers.forEach(async(element, index) => {
+		const $$ = await getHtml(serverURL[index].replace('amp;', ''));
 		const livideo = $$('script').html().match(/(?<=src=["'])([^"'])*/gm).map(it => {
 			return it.replace('..', '').replace('/stream/amz.php?', 'https://www.animefenix.com/stream/amz.php').replace('/stream/fl.php?v=https://', 'https://www.animefenix.com/redirect.php?player=22&code=');
 		});
 		
-		lista.push({name: element, url: livideo[0] });
-		if (lista.length == Nombre.length) {
+		lista.push({server: element, url: livideo[0] });
+		if (lista.length == servers.length) {
 			lista.map(it => {
-				if (it.name == 'M') it.name = 'Mega';
+				if (it.server == 'M') it.server = 'Mega';
 				return it;
 			});
-			res.json({ Id, lista });
+			res.json({ anime_id, lista });
 		}
-		
 	});
 };
 module.exports = VerEpisodio;
