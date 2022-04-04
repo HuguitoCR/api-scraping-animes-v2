272e8553af371e61e4ceb5d5bff1aef251bf059b
Kevin Arias <kevinjosuearias@hotmail.com>
2022-04-04 16:44:31 -0600

    Ahora se devuelve correctamente la informacion del directorio. Ya no presenta un arreglo como JSON object padre

--- app/Script/Animes/Directorio.js
+++ app/Script/Animes/Directorio.js
@@ -4,7 +4,7 @@ const Directorio = async(res) => {
 	const client = new Redis(process.env.REDIS_URL);
 	const reply = await client.get('directorio');
 	client.quit();
-	res.json(JSON.parse(reply));
+	res.json({ Directorio: JSON.parse(reply) });
 };
 
 module.exports = Directorio;
