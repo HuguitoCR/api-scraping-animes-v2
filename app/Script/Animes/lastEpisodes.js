c22264799a07bc03f4d97ae4d1f682371e932e61
HuguitoCR <h.mejias.ugalde53@gmail.com>
2022-04-15 14:24:25 -0600

    se arreglo name_id de lastepisodes

--- app/Script/Animes/lastEpisodes.js
+++ app/Script/Animes/lastEpisodes.js
@@ -5,7 +5,7 @@ const LastEpisodes = async(res) => {
 	const client = new Redis(process.env.REDIS_URL);
 	const reply = await client.get('lastEpisodes');
 
-	if (reply) {
+	if (!reply) {
 		client.quit();
 		res.json({ LastEpisodes: JSON.parse(reply), source: 'cache'}); 
 	}
@@ -19,8 +19,14 @@ const LastEpisodes = async(res) => {
 				title: $(this).find('div.overtitle').text().split('\n').join(''),
 				img: $(this).find('img').attr('src'),
 				episode: $(this).find('div.overepisode').text().split('\n').join(''),
-				name_id: $(this).find('img').attr('src').split('https://www.animefenix.com/')[1].split('/')[4].split('.jpg')[0].split('.png')[0]
+				// quitar cualquier cadarter no alfanumerica y signo de puntuacion
 
+				name_id: $(this).find('div.overtitle').text().trim().replace(/[.:!?¡¿(),'"]/g,'').replace('=','-').split(' ').join('-').toLowerCase().split(' ').join('').split('θ').join('-theta').split('---').join('-').split('--').join('-').split('story-').join('story').split('romantic-').join('romantic')
+
+				//.split('-').join('').split(' ').join('-').slice().join('').toLowerCase()
+				//.split('-').join('').slice(/[^a-zA-Z0-9]/g).split(' ').join('-').toLowerCase()
+				///[#_]/g
+				//.replace('.','').replace(':','').replace('=','-').toLowerCase()
 			};
 			LastEpisodes.push(episodio);
 		});
