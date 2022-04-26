e9a294541fc06f6f35cc7020061f2ff2bf015d1e
Kevin Arias <kevinjosuearias@hotmail.com>
2022-04-26 00:34:55 -0600

    add: nuevas reglas linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,11 @@ module.exports = {
 		'ecmaVersion': 'latest'
 	},
 	'rules': {
+		'brace-style': [
+			'error',
+			'1tbs',
+			{ 'allowSingleLine': true }
+		],
 		'indent': [
 			'error',
 			'tab'
@@ -25,6 +30,51 @@ module.exports = {
 			'error',
 			'always'
 		],
+		'dot-location': [
+			'error',
+			'property'
+		],
+		'comma-style': [
+			'error',
+			'last'
+		],
+		'comma-spacing': 'error',
+		'space-infix-ops': 'error',
+		'keyword-spacing': 'error',
+		'space-before-blocks': 'error',
+		'space-unary-ops': 'error',
+		'space-in-parens': [
+			'error',
+			'never'
+		],
+		'arrow-spacing': [
+			'warn',
+			{ 'before': true, 'after': true }
+		],
+		'space-before-function-paren': [
+			'error',
+			{ 'anonymous': 'never', 'named': 'never', 'asyncArrow': 'always' }
+		],
+		'object-curly-spacing': [
+			'error',
+			'always'
+		],
+		'array-bracket-spacing': [
+			'error',
+			'never'
+		],
+		'no-shadow': [
+			'error',
+			{ 'allow': ['err', 'resolve', 'reject'] }
+		],
+		'no-multiple-empty-lines': [
+			'error',
+			{ 'max': 2, 'maxEOF': 0, 'maxBOF': 0 }
+		],
+		'no-var': 'error',
+		'no-multi-spaces': 'error',
+		'no-trailing-spaces': 'error',
 		'no-case-declarations': 'off',
+		'yoda': 'error'
 	},
 };
